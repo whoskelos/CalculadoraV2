4b03f4a4dd4bccf7901ed6225e0747b5e7280204
Kelvin Guerrero <kelvinguerrero2199@gmail.com>
2021-04-21 13:25:38 +0200

    calculadora funcionando, falta corregir no repetir el punto una vez pulsado y los ceros a la izquierda

--- functions.js
+++ functions.js
@@ -1,21 +1,87 @@
 
+//variables
+var num1 = "";
+var num2 = "";
+var resultado = "";
+var semaforo = true;
+
 function mostrar_boton(boton) {
-    display = document.getElementById("pantalla").value;
-    document.getElementById("pantalla").value = display + boton.value;
-    noRepetirPunto(boton);
+    
+    document.getElementById("pantalla").value += boton.value;
+    if (semaforo) {
+        num1 = document.getElementById("pantalla").value;
+        console.log(num1);
+    }else{
+        num2 = document.getElementById("pantalla").value;
+        console.log(num2);
+    }
+    
+}
+
+//al momento de presionar un operador el semaforo cambia a false
+//empieza a guardar los nuevos numeros presionados en la variable
+//num2
+
+function coger_operador(oper) {
+    borrar_calcu();
+    operador = oper.value
+    semaforo = false;
+    console.log("operador:",operador);
 }
 
 function borrar_calcu() {
-    document.getElementById("pantalla").value = "0";
+    document.getElementById("pantalla").value = "";
 }
 
-function noRepetirPunto(valor) {
-    if (valor == '.') {
-        var texto = document.getElementById("pantalla").value;
-        if (texto.indexOf('.')<0) {
-            document.getElementById("pantalla").value +=valor;
-        }else{
-            document.getElementById("pantalla").value += valor;
-        }
+function limpiar_varibles() {
+    num1 = "0";
+    num2 = "0";
+    semaforo = true;
+}
+
+function mostrar_resultado() {
+    switch (operador) {
+        case "+": 
+        resultado = parseFloat(num1) + parseFloat(num2);
+        document.getElementById("pantalla").value = resultado;
+        num1= resultado.toString();
+        limpiar_varibles();
+        console.log(resultado);
+            break;
+        case "-": 
+        resultado = parseFloat(num1) - parseFloat(num2);
+        document.getElementById("pantalla").value = resultado;
+        num1= resultado.toString();
+        limpiar_varibles();
+        console.log(resultado);
+            break;
+        case "x": 
+        resultado = parseFloat(num1) * parseFloat(num2);
+        document.getElementById("pantalla").value = resultado;
+        num1= resultado.toString();
+        limpiar_varibles();
+        console.log(resultado);
+            break;
+        case "/": 
+        resultado = parseFloat(num1) / parseFloat(num2);
+        document.getElementById("pantalla").value = resultado;
+        num1= resultado.toString();
+        limpiar_varibles();
+        console.log(resultado);
+            break;
+    
+        default:
+            break;
     }
 }
+
+// function noRepetirPunto(valor) {
+//     if (valor == '.') {
+//         var texto = document.getElementById("pantalla").value;
+//         if (texto.indexOf('.')<0) {
+//             document.getElementById("pantalla").value +=valor;
+//         }else{
+//             document.getElementById("pantalla").value += valor;
+//         }
+//     }
+// }
